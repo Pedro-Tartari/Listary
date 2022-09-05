74e482439e94cd42461105a8079dbf2dd07b3f20
Luis Henrique de Camargo Gonçalves <81198576+LuisHenriqueCG@users.noreply.github.com>
2022-09-05 20:21:50 -0300

    Header em todas as telas
    - adicionado o header nas telas

--- app/src/main/java/com/example/listary/MainActivity.java
+++ app/src/main/java/com/example/listary/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.listary.view.MenuListaryActivity;
 import com.example.listary.view.registerForm.Register;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
@@ -30,9 +31,16 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+
+
             case R.id.register:
                 startActivity(new Intent(this, Register.class));
                 break;
+
+            case R.id.btEntrar:
+                startActivity(new Intent(this, MenuListaryActivity.class));
         }
+
+
     }
 }
