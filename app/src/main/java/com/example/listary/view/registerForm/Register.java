d3de77e51d5dd2381ccd8537d4ede2e22a93c637
Pedro Tartari <ph.tartari@gmail.com>
2022-09-05 15:44:24 -0300

    Feito a ligação e envio de dados
    - feito todo o backend da criação de um usuario
    - isso envolve validações

--- app/src/main/java/com/example/listary/view/registerForm/Register.java
+++ app/src/main/java/com/example/listary/view/registerForm/Register.java
@@ -1,20 +1,34 @@
 package com.example.listary.view.registerForm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.listary.MainActivity;
 import com.example.listary.R;
+import com.example.listary.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private EditText edUserName,edEmail,edPassword;
     private Button btnRegister;
+    private String userName, email, password;
+    private boolean isAllFieldsChecked = false;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +36,26 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         setViewId();
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isAllFieldsChecked = CheckAllFields();
+
+                if (isAllFieldsChecked) {
+                    creatUser();
+                    startActivity(new Intent(Register.this, MainActivity.class));
+                }
+            }
+
+        });
+
     }
+
+
 
     private void setViewId() {
         edUserName = findViewById(R.id.edUserName);
@@ -35,11 +65,48 @@ public class Register extends AppCompatActivity {
     }
 
 
-    public void UserCad(View view) {
-        String user = edUserName.getText().toString();
-        String email = edEmail.getText().toString();
-        String password = edPassword.getText().toString();
 
 
+    private boolean CheckAllFields() {
+        userName = edUserName.getText().toString();
+        email = edEmail.getText().toString();
+        password = edPassword.getText().toString();
+
+        if (userName.length() == 0) {
+            edUserName.setError("This field is required");
+            return false;
+        }
+
+        if (email.length() == 0) {
+            edEmail.setError("Email is required");
+            return false;
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edEmail.setError("Informe um email valido");
+            return false;
+        }
+
+        if (password.length() == 0) {
+            edPassword.setError("Password is required");
+            return false;
+        } else if (password.length() < 8) {
+            edPassword.setError("Password must be minimum 8 characters");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void creatUser() {
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            User user = new User(userName, email, password);
+                            db.collection("users")
+                                    .add(user);
+                        }
+                    }
+                });
     }
 }
