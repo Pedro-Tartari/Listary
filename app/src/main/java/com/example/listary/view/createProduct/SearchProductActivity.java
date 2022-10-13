3fa6b72ce3ddf0ac0c1f87d82726999ce5ff0650
Leonardo Torri Maito <80430708+LeonardoMaito@users.noreply.github.com>
2022-10-13 10:26:14 -0300

    Criação dos Adapters
    Criado o de produto apenas

--- app/src/main/java/com/example/listary/view/createProduct/SearchProductActivity.java
+++ app/src/main/java/com/example/listary/view/createProduct/SearchProductActivity.java
@@ -2,6 +2,9 @@ package com.example.listary.view.createProduct;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,15 +15,63 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.listary.R;
+import com.example.listary.adapters.ProductsAdapter;
+import com.example.listary.model.Product;
 import com.example.listary.view.loginForm.Login;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class SearchProductActivity extends AppCompatActivity {
+
+    private ProductsAdapter listAdapter;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference docRef =
+            db.collection("data")
+                    .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                    .collection("product");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_products);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,
+
+                DividerItemDecoration.VERTICAL);
+
+        RecyclerView recyclerView = findViewById(R.id.rvProducts);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        Query query = docRef.orderBy("name",
+                Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Product> options =
+                new FirestoreRecyclerOptions.Builder<Product>()
+                        .setQuery(query, Product.class)
+                        .build();
+
+        listAdapter = new ProductsAdapter(options);
+        recyclerView.setAdapter(listAdapter);
+        listAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        listAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        listAdapter.stopListening();
     }
 
     @Override
