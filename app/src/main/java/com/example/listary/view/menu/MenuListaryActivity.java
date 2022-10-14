4ba3d1d822018446d4d7c572b4596608dea07a88
Pedro Tartari <ph.tartari@gmail.com>
2022-10-14 20:21:53 -0300

    criação reservedid
    - so isso mesmo

--- app/src/main/java/com/example/listary/view/menu/MenuListaryActivity.java
+++ app/src/main/java/com/example/listary/view/menu/MenuListaryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,19 +13,57 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.listary.R;
+import com.example.listary.model.ReservedId;
 import com.example.listary.view.Pantry.PantryActivity;
 import com.example.listary.view.createProduct.SearchProductActivity;
 import com.example.listary.view.loginForm.Login;
 import com.example.listary.view.newList.NewListActivity;
 import com.example.listary.view.historic.HistoricActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MenuListaryActivity extends AppCompatActivity {
+
+    private String uID;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_listary);
+        uID = user.getUid();
+
+
+        db.collection("data").document(uID).collection("reservedID")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if(task.getResult().size() > 0) {
+                                for (DocumentSnapshot document : task.getResult()) {
+                                    Log.d("Resultado", "Room already exists, start the chat");
+                                }
+                            } else {
+                                DocumentReference idReference = db.collection("data")
+                                        .document(uID).collection("reservedID")
+                                        .document("reservedProductId");
+
+                                ReservedId reservedId = new ReservedId(0);
+                                idReference.set(reservedId);
+                            }
+                        } else {
+                            Log.d("FTAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     @Override
